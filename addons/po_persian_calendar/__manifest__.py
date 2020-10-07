148595f0c74ae04c719edfc254d17c1d5684327b
BabakMahmoudi <babak@gnco.ir>
2020-10-07 09:47:23 +0330

    calendar added to user perefernces

--- addons/po_persian_calendar/__manifest__.py
+++ addons/po_persian_calendar/__manifest__.py
@@ -20,19 +20,8 @@
     'depends': ['web'
     ],
     'data': [
-        # 'data/account_payment_method_data.xml',
-        # 'data/ir_actions_server_data.xml',
-        # 'wizard/account_check_action_wizard_view.xml',
-        # 'wizard/print_pre_numbered_checks_view.xml',
-        # 'wizard/res_config_settings_view.xml',
         'views/templates.xml',
-        # 'views/account_check_view.xml',
-        # 'views/account_journal_dashboard_view.xml',
-        # 'views/account_journal_view.xml',
-        # 'views/account_checkbook_view.xml',
-        # 'views/account_chart_template_view.xml',
-        # 'security/ir.model.access.csv',
-        # 'security/account_check_security.xml',
+        'views/UserPreferences_Views.xml',
     ],
     'demo': [
     ],
--- addons/po_persian_calendar/globals.py
+++ addons/po_persian_calendar/globals.py
@@ -6,9 +6,11 @@ if TYPE_CHECKING:
     from odoo.addons.base.models.res_partner import Partner
     from odoo.addons.base.models.ir_http import IrHttp
     from odoo.addons.web.models.ir_http import Http
+    from odoo.addons.base.models.res_users import Users
 else:
     Partner = models.Model
     IrHttp = models.AbstractModel
     Http = models.AbstractModel
+    Users = models.Model
 
 
